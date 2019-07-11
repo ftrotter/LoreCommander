b474323d5c01d9e0c130b35e6a3fe3be025b11e0
Fred Trotter <fred.trotter@gmail.com>
2019-07-11 04:25:00 +0000

    get images uris too

--- app/Console/Commands/SlowScrapeScryfall.php
+++ app/Console/Commands/SlowScrapeScryfall.php
@@ -91,16 +91,11 @@ class SlowScrapeScryfall extends Command
 
 
 		//prep for the next loop
-		$set_code_list[$DURCmtgset->id] = $this_set['code']; //we will use the to download the cards a set at a time in a moment		
-
-	}
-	
-	foreach($set_code_list as $mtgset_id => $this_set_code){
-		if($this_set_code == 'dka'){
-			$dka_mtgset_id = $mtgset_id;
-			$dka_set_code = $this_set_code;
+		if($this_set['card_count'] > 0){ //apparently a set can be empty. Not sure the usefulness of this..
+			$set_code_list[$DURCmtgset->id] = $this_set['code']; //we will use the to download the cards a set at a time in a moment		
 		}
 	}
+	
 
 
 	//some meta mapping data to help us map each card to the right local variables.
@@ -173,154 +168,176 @@ class SlowScrapeScryfall extends Command
 			'W' => 'white',	
 		];
 
-	$mtgset_id = $dka_mtgset_id;
+		$image_lookup = [
+			'small',
+			'normal',
+			'large',
+			'png',
+			'art_crop',
+			'border_crop',
+		];
 
-	$cards = \App\ScryfallAPI::getAllCardsInSet($dka_set_code);
-
-	foreach($cards as $this_outer_card){
-
-		//first we consider the cardfaces...
+	foreach($set_code_list as $mtgset_id => $this_set_code){
 	
-		$card_loop = [];
+		echo "Working on setcode:$this_set_code\n";
+		
+		$cards = \App\ScryfallAPI::getAllCardsInSet($this_set_code);
 
-		if(isset($this_outer_card['card_faces'])){
-			//then this card has more than one card face..
-			$name = $this_outer_card['name'];
-			echo "Looping over $name\n";
-			$is_double_face = true;
-			foreach($this_outer_card['card_faces'] as $cardface_index => $this_face){
-				$this_face['cardface_index'] = $cardface_index;
-				$card_loop[] = array_merge($this_face,$this_outer_card); //will flatten the card face and the card into one big thing...
-			}
-		}else{
-			$is_double_face = false;
-			$this_outer_card['cardface_index'] = 0; //because it just has the one..
-			$card_loop[] = $this_outer_card; //we only have 1 card <-> cardface
-		}	
+		foreach($cards as $this_outer_card){
+
+			//first we consider the cardfaces...
 	
-		foreach($card_loop as $this_card){
+			$card_loop = [];
 
-			$illustration_id = $this_card['illustration_id'];
-			$scryfall_id = $this_card['id'];
-			$cardface_index = $this_card['cardface_index'];
-
-			if($is_double_face){
-
-				echo "\tscrayfall_id:$scryfall_id	\tcardface_index:$cardface_index \tillustration_id:$illustration_id\n";		
-				
-			}
-
-			$card_fill = []; //this should be the same for both card loops...
-			$cardface_fill = []; //blank this out for every card loop... 
-
-			//these we rename..
-			$card_fill['scryfall_id'] = $scryfall_id;
-			$card_fill['mtgset_id'] = $mtgset_id;
-			$card_fill['scryfall_api_uri'] = $this_card['uri'];		
-			$card_fill['scryfall_web_uri'] = $this_card['scryfall_uri'];
-
-			if(isset($this_card['variation_of'])){
-				$card_fill['variation_of_scryfall_id'] = $this_card['variation_of'];
+			if(isset($this_outer_card['card_faces'])){
+				//then this card has more than one card face..
+				$name = $this_outer_card['name'];
+				echo "Looping over $name\n";
+				$is_double_face = true;
+				foreach($this_outer_card['card_faces'] as $cardface_index => $this_face){
+					$this_face['cardface_index'] = $cardface_index;
+					$card_loop[] = array_merge($this_face,$this_outer_card); //will flatten the card face and the card into one big thing...
+				}
 			}else{
-				$card_fill['variation_of_scryfall_id'] = null;
-			}
-
-			foreach($card_convert_to_tiny_int as $is_postfix){
-				if(isset($this_card[$is_postfix])){
-					if($this_card[$is_postfix]){
-						$card_fill["is_$is_postfix"] = 1;
+				$is_double_face = false;
+				$this_outer_card['cardface_index'] = 0; //because it just has the one..
+				$card_loop[] = $this_outer_card; //we only have 1 card <-> cardface
+			}	
+	
+			foreach($card_loop as $this_card){
+	
+				if(isset($this_card['illustration_id'])){	
+					$illustration_id = $this_card['illustration_id'];
+				}else{
+					$illustration_id = 0;
+				}
+				$scryfall_id = $this_card['id'];
+				$cardface_index = $this_card['cardface_index'];
+	
+				if($is_double_face){
+	
+					echo "\tscrayfall_id:$scryfall_id	\tcardface_index:$cardface_index \tillustration_id:$illustration_id\n";		
+					
+				}
+	
+				$card_fill = []; //this should be the same for both card loops...
+				$cardface_fill = []; //blank this out for every card loop... 
+	
+				//these we rename..
+				$card_fill['scryfall_id'] = $scryfall_id;
+				$card_fill['mtgset_id'] = $mtgset_id;
+				$card_fill['scryfall_api_uri'] = $this_card['uri'];		
+				$card_fill['scryfall_web_uri'] = $this_card['scryfall_uri'];
+	
+				if(isset($this_card['variation_of'])){
+					$card_fill['variation_of_scryfall_id'] = $this_card['variation_of'];
+				}else{
+					$card_fill['variation_of_scryfall_id'] = null;
+				}
+	
+				foreach($card_convert_to_tiny_int as $is_postfix){
+					if(isset($this_card[$is_postfix])){
+						if($this_card[$is_postfix]){
+							$card_fill["is_$is_postfix"] = 1;
+						}else{
+							$card_fill["is_$is_postfix"] = 0;
+						}
 					}else{
-						$card_fill["is_$is_postfix"] = 0;
+						$card_fill["is_$is_postfix"] = 0;//missing values get zero
 					}
-				}else{
-					$card_fill["is_$is_postfix"] = 0;//missing values get zero
 				}
-			}
-
-			//
-			foreach($card_field_mirror as $this_field){
-				if(isset($this_card[$this_field])){
-					$card_fill[$this_field] = $this_card[$this_field]; //pretty obvious
-				}else{
-					$card_fill[$this_field] = null; //should be accepted by the DB
+	
+				//
+				foreach($card_field_mirror as $this_field){
+					if(isset($this_card[$this_field])){
+						$card_fill[$this_field] = $this_card[$this_field]; //pretty obvious
+					}else{
+						$card_fill[$this_field] = null; //should be accepted by the DB
+					}
 				}
-			}
-
-			//flatten the legalities i.e. legacy, modern and standard
-			if(isset($this_card['legalities'])){
-				foreach($this_card['legalities'] as $legal_in => $legal_status){
-					$legal_score = $legal_lookup[$legal_status];
-					$card_fill["legal_$legal_in"] = $legal_score; //it is 0 by dfault..
+	
+				//flatten the legalities i.e. legacy, modern and standard
+				if(isset($this_card['legalities'])){
+					foreach($this_card['legalities'] as $legal_in => $legal_status){
+						$legal_score = $legal_lookup[$legal_status];
+						$card_fill["legal_$legal_in"] = $legal_score; //it is 0 by dfault..
+					}
 				}
-			}
-
-			//flattent the games array i.e. mtgo, arena, print
-			if(isset($this_card['games'])){
-				foreach($this_card['games'] as $this_game){
-					$card_fill["is_game_$this_game"] = 1; //it is 0 by dfault..
+	
+				//flattent the games array i.e. mtgo, arena, print
+				if(isset($this_card['games'])){
+					foreach($this_card['games'] as $this_game){
+						$card_fill["is_game_$this_game"] = 1; //it is 0 by dfault..
+					}
 				}
-			}
-
-			$DURC_card = \App\card::firstOrNew(['scryfall_id' => $scryfall_id]);
-			$DURC_card->fill($card_fill);
-			$DURC_card->save();
-
-			$card_id = $DURC_card->id; //need this to save the card face..
-		
-			$cardface_fill['card_id'] = $card_id;
+	
+				$DURC_card = \App\card::firstOrNew(['scryfall_id' => $scryfall_id]);
+				$DURC_card->fill($card_fill);
+				$DURC_card->save();
+	
+				$card_id = $DURC_card->id; //need this to save the card face..
 			
-			//first lets copy over the fields that will not change at all.. 
-			foreach($cardface_field_mirror as $this_field){
-				if(isset($this_card[$this_field])){
-					$cardface_fill[$this_field] = $this_card[$this_field]; //pretty obvious
-				}else{
-					$cardface_fill[$this_field] = null; //should be accepted by the DB
-				}
-			}
-		
-		 	//convert the booleans to our is_ tinyint notation...
-			foreach($cardface_convert_to_tiny_int as $is_postfix){
-				if(isset($this_card[$is_postfix])){
-					if($this_card[$is_postfix]){
-						$cardface_fill["is_$is_postfix"] = 1;
+				$cardface_fill['card_id'] = $card_id;
+				
+				//first lets copy over the fields that will not change at all.. 
+				foreach($cardface_field_mirror as $this_field){
+					if(isset($this_card[$this_field])){
+						$cardface_fill[$this_field] = $this_card[$this_field]; //pretty obvious
 					}else{
-						$cardface_fill["is_$is_postfix"] = 0;
+						$cardface_fill[$this_field] = null; //should be accepted by the DB
 					}
-				}else{
-					$cardface_fill["is_$is_postfix"] = 0;//missing values get zero
 				}
-			}
-
-			//now we calculate our is_color variables...
-			$has_color = false;
-			$color_count = 0;
-			foreach($this_card['color_identity'] as $has_this_color){
-				$color_name = $color_lookup[$has_this_color];
-				$cardface_fill["is_color_$color_name"] = 1;				
-
-				$color_count++;
-				$has_color = true;
-			}
-		
-			if(!$has_color){
-				$cardface_fill["is_colorless"] = true;
-			}
-
-
-			$cardface_fill['color_count'] = $color_count;
-
-			//a card face is unique on card_id plus illustration_id... probably...
-			$DURC_cardface = \App\cardface::firstOrNew([
-							'card_id' => $card_id,
-							'illustration_id' => $illustration_id,
-							]);
-			$DURC_cardface->fill($cardface_fill);
-			$DURC_cardface->save();
-
-		}
-
-	}
+			
+			 	//convert the booleans to our is_ tinyint notation...
+				foreach($cardface_convert_to_tiny_int as $is_postfix){
+					if(isset($this_card[$is_postfix])){
+						if($this_card[$is_postfix]){
+							$cardface_fill["is_$is_postfix"] = 1;
+						}else{
+							$cardface_fill["is_$is_postfix"] = 0;
+						}
+					}else{
+						$cardface_fill["is_$is_postfix"] = 0;//missing values get zero
+					}
+				}
+	
+				//now we calculate our is_color variables...
+				$has_color = false;
+				$color_count = 0;
+				foreach($this_card['color_identity'] as $has_this_color){
+					$color_name = $color_lookup[$has_this_color];
+					$cardface_fill["is_color_$color_name"] = 1;				
+	
+					$color_count++;
+					$has_color = true;
+				}
+			
+				if(!$has_color){
+					$cardface_fill["is_colorless"] = true;
+				}
+	
+	
+				$cardface_fill['color_count'] = $color_count;
 	
 
-    }
+				foreach($this_card['image_uris'] as $image_type => $image_url){
+					$cardface_fill["image_url_$image_type"] = $image_url;
+				}
+
+
+				//a card face is unique on card_id plus illustration_id... probably...
+				$DURC_cardface = \App\cardface::firstOrNew([
+								'card_id' => $card_id,
+								'illustration_id' => $illustration_id,
+								]);
+				$DURC_cardface->fill($cardface_fill);
+				$DURC_cardface->save();
+	
+			} //end of cardface loop
+	
+		} //end of card loop
+		
+	} //end of set loop.
+
+    }//end of handler function
 }
