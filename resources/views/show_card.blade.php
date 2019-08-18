1c065c6fc205d4e92c9a2a6ae13c1f905f511f4a
Fred Trotter <fred.trotter@gmail.com>
2019-08-18 00:00:38 -0500

    get rulings

--- resources/views/show_card.blade.php
+++ resources/views/show_card.blade.php
@@ -43,8 +43,10 @@
   					.then(async function(myData) {
 						//console.log('before get_illustration');
 						var ill_img_list = await get_illustration_array(myData);
+						var rulings_list = await get_rulings_array(myData);
 						await sleep(2000); //doing this as the simplest way to ensure that my ill_img_list is really set
 						myData.ill_img_list = ill_img_list;
+						myData.rulings_list = rulings_list;
 						//console.log('before mustache to_html');
 						//console.log(myData);
 						var card_html = Mustache.to_html(tpl, myData);
@@ -54,7 +56,28 @@
 			}); // end get template
 		});  //end pusher channel bind
 
-	//
+	//get the rulings requires another API call..
+	async function get_rulings_array(card_data){
+
+		rulings_list = [];
+
+		fetch(card_data.rulings_uri)
+			.then(function(response){
+				 return response.json();
+				})
+			.then(function(rulings_data){
+				rulings_data.data.forEach(function(this_ruling) {
+					rulings_list.push(this_ruling);
+				});			
+			});
+
+		return(rulings_list);
+
+	}
+
+
+	// looks inside the card for the prints_search_url, and returns the list of images 
+	// from the various prints...
 	async function get_illustration_array(card_data){
 
 		img_list = [];
@@ -64,20 +87,16 @@
 				 return response.json();
 				})
 			.then(function(cards_data){
-		//		console.log('cards data');
-		//		console.log(cards_data);
 				cards_data.data.forEach(function(this_card) {
 					img_list.push(this_card['image_uris']['large']);
 				});			
 			});
 
-	//	console.log('this is img_list');	
-	//	console.log(img_list);
-
 		return(img_list);
 
-
 	}
+
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
