07119ca94412205481b20b802cf58ee46cf6c9cc
Fred Trotter <fred.trotter@gmail.com>
2019-08-25 17:28:42 -0500

    no more slim jquery

--- resources/views/genericLinker.blade.php
+++ resources/views/genericLinker.blade.php
@@ -52,7 +52,7 @@ This interface allows you to quickly tag sets of objects...
 
 </div>
 
-    <script src="/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="/js/jquery-3.3.1.min.js"></script>
     <script src="/js/popper.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/select2.min.js"></script>
