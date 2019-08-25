12b272de5042bf46a5796157e69ddd2cc74cb3e9
Fred Trotter <fred.trotter@gmail.com>
2019-08-25 16:58:13 -0500

    close braces

--- resources/views/genericLinker.blade.php
+++ resources/views/genericLinker.blade.php
@@ -38,7 +38,7 @@ This interface allows you to quickly tag sets of objects...
 
 <script type='text/javascript'>
 
-$('.{{durc_type_left')_div.select2({
+$('.{{durc_type_left}}')_div.select2({
   ajax: {
     	url: '/DURC/searchjson/{{durc_type_left}}/',
     	dataType: 'json'
