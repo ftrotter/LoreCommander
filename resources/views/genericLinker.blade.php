298bf6951502379419d78c4f0ad54650f3bc02df
Fred Trotter <fred.trotter@gmail.com>
2019-08-25 17:44:58 -0500

    send many values

--- resources/views/genericLinker.blade.php
+++ resources/views/genericLinker.blade.php
@@ -36,7 +36,7 @@ This interface allows you to quickly tag sets of objects...
 		<div class='col-md-3'>
 		LEft {{$durc_type_left}} goes here
 
-<select class='{{$durc_type_left}}_id form-control' multiple='' id='{{$durc_type_left}}_id' name='{{$durc_type_left}}_id'>
+<select class='{{$durc_type_left}}_id form-control' multiple='' id='{{$durc_type_left}}_id' name='{{$durc_type_left}}_id[]'>
 	</select>
 
 
