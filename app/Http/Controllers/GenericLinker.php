5e7f6e366991e5562578a00438308e8c87d4723b
Fred Trotter <fred.trotter@gmail.com>
2019-08-25 23:50:51 -0500

    create unique keys at outset

--- app/Http/Controllers/GenericLinker.php
+++ app/Http/Controllers/GenericLinker.php
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS $db.$link_table  (
 	`link_note` VARCHAR(255) DEFAULT NULL ,  
 	`created_at` DATETIME NOT NULL ,  
 	`updated_at` DATETIME NOT NULL ,    
-	PRIMARY KEY  (`id`)
+	PRIMARY KEY  (`id`),
 	UNIQUE KEY( $durc_type_left"."_id, $durc_type_right"."_id, $durc_type_tag"."_id )
 	) ENGINE = MyISAM 
 ;
