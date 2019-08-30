a9dd773d2fde8921ee8faf750a8def5911568777
Fred Trotter <fred.trotter@gmail.com>
2019-08-30 10:08:39 -0500

    durc mods

--- app/DURC/Models/cardface_person_atag.php
+++ app/DURC/Models/cardface_person_atag.php
@@ -106,7 +106,7 @@ CREATE TABLE `lore`.`cardface_person_atag` (
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `cardface_id` (`cardface_id`,`person_id`,`atag_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8
 */
 
 
