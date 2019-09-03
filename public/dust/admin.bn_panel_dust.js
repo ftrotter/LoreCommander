5c24dac852dd5a08b1f81d081201c01d4330bafc
Fred Trotter <fred.trotter@gmail.com>
2019-09-02 22:27:06 -0500

    ancient graph components

---
+++ public/dust/admin.bn_panel_dust.js
@@ -0,0 +1,1 @@
+(function(dust){dust.register("admin.bn_panel_dust",body_0);function body_0(chk,ctx){return chk.w("        <div class=\"panel panel-default\"><div class=\"panel-body\"><h5>Admin Node Panel</h5></div></div>");}body_0.__dustBody=!0;return body_0;})(dust);
---
+++ public/dust/wikilink.js
@@ -0,0 +1,1 @@
+(function(dust){dust.register("wikilink",body_0);function body_0(chk,ctx){return chk.w("<a data-toggle=\"modal\"  data-wiki=\"").f(ctx.get(["wikipage"], false),ctx,"h").w("\" class='wiki btn btn-link btn-xs'>?</a>");}body_0.__dustBody=!0;return body_0;})(dust);
