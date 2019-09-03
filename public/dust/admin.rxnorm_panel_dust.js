9a97ebd3bdeab5ff038615208b38205b10c73c80
Fred Trotter <fred.trotter@gmail.com>
2019-09-03 04:35:18 -0500

    no more dust

--- public/dust/admin.rxnorm_panel_dust.js
+++
@@ -1,1 +0,0 @@
-(function(dust){dust.register("admin.rxnorm_panel_dust",body_0);function body_0(chk,ctx){return chk.w("        <div class=\"panel panel-default\"><div class=\"panel-body\"><h5>Admin Node Panel</h5></div></div>");}body_0.__dustBody=!0;return body_0;})(dust);
