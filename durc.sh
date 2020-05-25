00810d51d263f8f47230c694b008021e05f54465
Fred Trotter <fred.trotter@gmail.com>
2020-05-25 13:02:28 -0500

    move to using durc.php

--- durc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm resources/views/DURC/* -rf
-php artisan DURC:mine --squash --DB=lore --DB=DURC_aaa --DB=DURC_irs --DB=DURC_northwind_model --DB=DURC_northwind_data
-php artisan DURC:write  
-cp routes/starting.web.php routes/web.php
-cat routes/web.durc.php | tail -n +2 >> routes/web.php
-cat routes/ending.web.php >> routes/web.php
-composer clear-cache
-composer dump-autoload
