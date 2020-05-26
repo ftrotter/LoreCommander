d2bcbf567a2257ec7c8224cfc920c1f15545d372
Fred Trotter <fred.trotter@gmail.com>
2020-05-25 19:38:24 -0500

    less verbose output

--- durc.php
+++ durc.php
@@ -30,20 +30,13 @@
 		exit();
 	}
 
-$commands = [
+$main_commands = [
 	"php artisan DURC:mine --squash $db_arg", //mine the databases
 	"php artisan DURC:write ", //generate the sourcecode automagically
-	//TODO this is clearly not the right way to do this...
-	"cp routes/starting.web.php routes/web.php", //copy the route prefixes routes over... 
-	"cat routes/web.durc.php | tail -n +2 >> routes/web.php", //copy the auto generated routes over
-	"cat routes/ending.web.php >> routes/web.php", //copy the route closing contentn
-	//these two are just for good measure... 
-	"composer clear-cache", //make composer see the new files
-	"composer dump-autoload", //and refresh the composer cache...
 ];
 
-
-	foreach($commands as $this_command){
+	//for the main commands, we want to see the output, so we use system()
+	foreach($main_commands as $this_command){
 		echo "Running: $this_command\n";
 		system($this_command,$return_status);
 		
@@ -55,4 +48,26 @@ $commands = [
 	}
 
 
+$clean_up_commands = [
+	//TODO this is clearly not the right way to do this...
+	"cp routes/starting.web.php routes/web.php", //copy the route prefixes routes over... 
+	"cat routes/web.durc.php | tail -n +2 >> routes/web.php", //copy the auto generated routes over
+	"cat routes/ending.web.php >> routes/web.php", //copy the route closing contentn
+	//these two are just for good measure... 
+	"composer clear-cache > /dev/null 2>&1", //make composer see the new files
+	"composer dump-autoload > /dev/null 2>&1", //and refresh the composer cache...
+];
 
+	//for the cleanup commands, we just want them to silently run... so we use exec without an echo
+	foreach($clean_up_commands as $this_command){
+		//echo "Running: $this_command\n";
+		exec($this_command,$output,$return_status);
+		
+		//DURC tries to respect error codes for returned status.. so this works as expected.
+		if($return_status > 0){ //then it retueded an error!!
+			echo "Error: $this_command failed... returned $return_status stopping\n";
+			exit(100);
+		}
+	}
+
+echo "clean up tasks finished. all done\n";
