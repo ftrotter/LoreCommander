13f3a484fabfac8a060a310d10f310abfdf26452
Fred Trotter <fred.trotter@gmail.com>
2020-06-11 03:52:57 +0000

    make everything in the careset group

--- scripts/fix_permissions.sh
+++ scripts/fix_permissions.sh
@@ -2,6 +2,11 @@
 
 # change the group owner of the all the things to 'careset'
 chgrp careset * -R
+# change the ownership of the storage directory to be writeable by apache
+#chgrp www-data storage -R
+#better is to ensure that www-data is a member of the careset user group..
+usermod -a -G careset www-data
+
 # ensure that members of the careset group can read, and write to the files. Ensure that new files have the same permission with the sticky bit
 chmod g+rw * -R
 # add the +s permission, but only to the directories..
