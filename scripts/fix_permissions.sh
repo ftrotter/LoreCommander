5225564ec5e2f1ba1d9a7f7b7ae2b0c3412fbf73
Fred Trotter <fred.trotter@gmail.com>
2020-09-27 20:28:19 +0000

    target that git directory

--- scripts/fix_permissions.sh
+++ scripts/fix_permissions.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# change the group owner of the all the things to 'careset'
+echo "changing all group ownership to careset"
 chgrp careset * -R
-# change the ownership of the storage directory to be writeable by apache
-#chgrp www-data storage -R
-#better is to ensure that www-data is a member of the careset user group..
-usermod -a -G careset www-data
 
-# ensure that members of the careset group can read, and write to the files. Ensure that new files have the same permission with the sticky bit
+echo "changing the .git directory specifically"
+chgrp careset .git/* -R
+chgrp careset .git
+
+echo "change the ownership of the storage directory to be writeable by apache"
+chown www-data:careset storage -R
+
+echo "ensure that members of the careset group can read, and write to the files. Ensure that new files have the same permission with the sticky bit"
 chmod g+rw * -R
-# add the +s permission, but only to the directories..
+chmod g+rw .git/* -R
+chmod g+rw .git
+
+echo "add the +s permission, but only to the directories.."
 find ./ -type d | xargs chmod g+s
+
+echo "This only works if your user is a member of the careset group. Do you see your user listed below?"
+members careset
+
