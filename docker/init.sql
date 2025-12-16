030f177066e529abc15d32efdfeca79c1f4bfbde
Fred Trotter <fred.trotter@gmail.com>
2025-12-15 22:46:22 -0500

    fixing the db permissions

--- docker/init.sql
+++ docker/init.sql
@@ -1,21 +1,5 @@
--- Grant permissions for Zermelo cache and config databases
--- These databases are created dynamically by Zermelo
-GRANT ALL PRIVILEGES ON `_zermelo_cache`.* TO 'loreuser'@'%';
-GRANT ALL PRIVILEGES ON `_zermelo_config`.* TO 'loreuser'@'%';
-
--- Grant permissions for DURC test databases
--- These are loaded from SQL files in setup_db/
-GRANT ALL PRIVILEGES ON `DURC_aaa`.* TO 'loreuser'@'%';
-GRANT ALL PRIVILEGES ON `DURC_irs`.* TO 'loreuser'@'%';
-GRANT ALL PRIVILEGES ON `DURC_northwind_data`.* TO 'loreuser'@'%';
-GRANT ALL PRIVILEGES ON `DURC_northwind_model`.* TO 'loreuser'@'%';
-
--- Grant permissions for lore and wallpaper databases
-GRANT ALL PRIVILEGES ON `lore`.* TO 'loreuser'@'%';
-GRANT ALL PRIVILEGES ON `wallpaper`.* TO 'loreuser'@'%';
-GRANT ALL PRIVILEGES ON `wallpaper_url`.* TO 'loreuser'@'%';
-
--- Allow the user to create new databases (needed for setup script)
-GRANT CREATE ON *.* TO 'loreuser'@'%';
+-- Grant root-level permissions to loreuser
+-- This gives full administrative access to all databases
+GRANT ALL PRIVILEGES ON *.* TO 'loreuser'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
